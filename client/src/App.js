8650c9e2d2d79e8654e4350156fe26e7c279d21d
Santino Valenzuela <s0nnyv123@gmail.com>
2022-03-19 15:52:18 -0400

    Update App component creating the form and styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>CRUD MOVIE REVIEW APPLICATION</h1>
+
+      <div className="form">
+        <label>Movie Name</label>
+        <input type="text" name="movieName" />
+
+        <label>Movie Review</label>
+        <input type="text" name="review" />
+
+        <button>Submit</button>
+      </div>
 
     </div>
   );
