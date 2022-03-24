f4ca9189ef2d0e2b55edea9b44ab7221d6b710ad
Santino Valenzuela <s0nnyv123@gmail.com>
2022-03-23 21:39:15 -0400

    Updated App.js to add title of the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [movieName, setMovieName] = useState('');
+  const [review, setReview] = useState('');
+
   return (
     <div className="App">
       <h1>CRUD MOVIE REVIEW APPLICATION</h1>
-
       <div className="form">
         <label>Movie Name</label>
-        <input type="text" name="movieName" />
+        <input type="text" name="movieName" onChange={(e) => setMovieName(e.target.value)} />
 
         <label>Movie Review</label>
-        <input type="text" name="review" />
+        <input type="text" name="review" onChange={(e) => setReview(e.target.value)} />
 
         <button>Submit</button>
       </div>
