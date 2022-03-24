d83b16f728f9390d6a329371fdc6f940f12fc8a4
Santino Valenzuela <s0nnyv123@gmail.com>
2022-03-23 22:54:20 -0400

    Create axios request to send data to the backend on click submit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
 
   const [movieName, setMovieName] = useState('');
   const [review, setReview] = useState('');
+
+  // Make an Axios/post request to send data to the backend
+  const submitReview = (e) => {
+    console.log('submitReview');
+    console.log(e);
+
+    console.log(movieName);
+    console.log(review);
+
+    Axios.post('http://localhost:5000/api/insert', { movieName: movieName, movieReview: review }).then(() => {
+      console.log('Successful insert!');
+    });
+  }
 
   return (
     <div className="App">
@@ -16,7 +30,7 @@ function App() {
         <label>Movie Review</label>
         <input type="text" name="review" onChange={(e) => setReview(e.target.value)} />
 
-        <button>Submit</button>
+        <button onClick={submitReview}>Submit</button>
       </div>
 
     </div>
